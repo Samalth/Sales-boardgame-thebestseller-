a412c43b412f1146f169083b4f726d3d0697a99b
samuelvde <samuel.vandeneijnde@student.hu.nl>
2024-02-16 10:34:16 +0100

    Dice made

---
+++ script.js
@@ -0,0 +1,20 @@
+let images = ["Dia1.JPG","Dia2.JPG","Dia3.JPG","Dia4.JPG","Dia5.JPG","Dia6.JPG"]
+
+let dice = document.querySelector("img")
+
+function roll(){
+    dice.classList.add("shake")
+    
+    let interval = setInterval(function() {
+        let diceValue = Math.floor(Math.random() * 6);
+        dice.setAttribute("src", images[diceValue]);
+    }, 100);
+
+    setTimeout(function(){
+        clearInterval(interval)
+        dice.classList.remove("shake")
+        let diceValue = Math.floor(Math.random()*6)
+        document.querySelector("#die-1").setAttribute("src", images[diceValue])
+        document.querySelector("#total").innerHTML = "You rolled: " + (diceValue + 1)
+    }, 1000)
+}
