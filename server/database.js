0ac66eb929d462bfdbf155a9109a8da81b797fcd
Sam Alex <sam.thiers@student.hu.nl>
2024-03-22 12:55:22 +0100

    Production update

--- server/database.js
+++ server/database.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "questionstable"
+    database: "thebestseller"
 });
 connection.connect();
 
