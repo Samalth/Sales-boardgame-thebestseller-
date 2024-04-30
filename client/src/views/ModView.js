8908576957a133385af95f56aaa54658d351ec32
Kees vd Zandschulp <kees.vandezandschulp@student.hu.nl>
2024-04-30 11:29:40 +0200

    Only one button selectable for assigning points in ModView.js

--- client/src/views/ModView.js
+++ client/src/views/ModView.js
@@ -149,7 +149,7 @@ export function ModView() {
     const [position, setPosition] = useState("8-5")
     const [submittedAnswer, setSubmittedAnswer] = useState('')
     const [diceValue, setDiceValue] = useState(1);
-    const [selectedPoints, setSelectedPoints] = useState([]);
+    const [selectedPoints, setSelectedPoints] = useState(null);
 
     useEffect(() => {
         const numPlayers = sortedUserData.length;
@@ -213,7 +213,7 @@ export function ModView() {
         };
     })
 
-    const handleUpdatePoints = (buttonPoints) => {
+/*    const handleUpdatePoints = (buttonPoints) => {
         setSelectedPoints((prevSelectedPoints) => {
             if (prevSelectedPoints.includes(buttonPoints)) {
                 return prevSelectedPoints.filter((point) => point !== buttonPoints);
@@ -221,6 +221,10 @@ export function ModView() {
                 return [...prevSelectedPoints, buttonPoints];
             }
         });
+    };*/
+
+    const handleUpdatePoints = (points) => {
+        setSelectedPoints(points);
     };
 
 
@@ -280,11 +284,11 @@ export function ModView() {
                         <div className='correctAnswerBox'></div>
                         <div className='assignScoreText'> Assign score: </div>
                         <div className='scoreButtons'>
-                            <button className={selectedPoints.includes(0) ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(0)}> 0 </button>
-                            <button className={selectedPoints.includes(5) ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(5)}> 5 </button>
-                            <button className={selectedPoints.includes(10) ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(10)}> 10 </button>
-                            <button className={selectedPoints.includes(15) ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(15)}> 15 </button>
-                            <button className={selectedPoints.includes(20) ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(20)}> 20 </button>
+                            <button className={selectedPoints === 0 ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(0)}> 0 </button>
+                            <button className={selectedPoints === 5 ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(5)}> 5 </button>
+                            <button className={selectedPoints === 10 ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(10)}> 10 </button>
+                            <button className={selectedPoints === 15 ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(15)}> 15 </button>
+                            <button className={selectedPoints === 20 ? 'selected points' : 'points'} onClick={() => handleUpdatePoints(20)}> 20 </button>
                         </div>
                         <button className='submitScoreButton' onClick={() => { setShowPopup(false); handleSubmitPoints(); }}>Submit</button>
                     </div>
