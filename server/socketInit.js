3bccc92cf0f96c5f51dd99d5172b400e28cd9a3a
Sam Alex <sam.thiers@student.hu.nl>
2024-06-07 10:46:23 +0200

    8.0.0 Localhost version STABLE
    - Full application
    - install instructions in readme!

--- server/socketInit.js
+++ server/socketInit.js
@@ -1,19 +1,19 @@
 const { Server } = require('socket.io');
 
 module.exports = function(server) {
-    // return new Server(server, {
-    //     cors: {
-    //         origin: 'http://localhost:3000',
-    //         methods: ['GET', 'POST']
-    //     }
-    // });
     return new Server(server, {
-        connectionStateRecovery: true,
-        path: '/server',
         cors: {
-            origin: 'https://thebestsellergame.online',
+            origin: 'http://localhost:3000',
             methods: ['GET', 'POST']
         }
     });
+    // return new Server(server, {
+    //     connectionStateRecovery: true,
+    //     path: '/server',
+    //     cors: {
+    //         origin: 'https://thebestsellergame.online',
+    //         methods: ['GET', 'POST']
+    //     }
+    // });
 };
 
