19d7580cdf60b4925d8081abc4c3dd6f96e6bcf2
Sam Alex <sam.thiers@student.hu.nl>
2024-06-07 11:25:46 +0200

    8.0.0 Localhost version STABLE
    - Full application
    - install instructions in readme!

--- client/src/client.js
+++ client/src/client.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-// export const socket = io.connect('http://localhost:3001')
+export const socket = io.connect('http://localhost:3001')
 
 // VOOR DEPLOYMENT
 // export const socket = io.connect({path: '/server', transports: ['websocket', 'polling']})
